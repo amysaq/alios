aed4c7a77483c00b1d8f94200b0a172abee83229
Anqi Shen <anqishen@Anqis-MacBook-Pro.local>
2018-06-27 09:46:27 +0800

    remove specjvm2008

--- specjvm2008/SPECjvm2008.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "***start Alipay_JDK7 benchmark***"
-cd ./SPECjvm2008
-rm ./results/* -rf
-~/build/linux-amd64/Alipay_JDK7/j2sdk-server-image/bin/java -jar SPECjvm2008.jar -ikv --lagom -bt 8 -ops 15 -i 5 -pf props/specjvm.properties
-echo "***start JDK6 benchmark***"
-/opt/taobao/java/bin/java -jar SPECjvm2008.jar -ikv --lagom -bt 8 -ops 15 -i 5 -pf props/specjvm.properties
-echo "***Benchmark Complete***"
